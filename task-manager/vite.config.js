3197a84890fe40b39926a4ce83605a46806a48aa
snehameganathan <snehameganath@gmail.com>
2025-11-20 21:24:32 +0530

    Fix Vercel base path

--- task-manager/vite.config.js
+++ task-manager/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // Required for Vercel
+  base: "/",   // REQUIRED for Vercel hosting
 })
